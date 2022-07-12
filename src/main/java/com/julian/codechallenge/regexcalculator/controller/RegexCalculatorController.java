7edd0b3140fbaf43718a407e5b56c91e90948fa0
juliangarciar <juliangarciargw@gmail.com>
2022-07-12 12:51:35 +0200

    Security setup

--- src/main/java/com/julian/codechallenge/regexcalculator/controller/RegexCalculatorController.java
+++ src/main/java/com/julian/codechallenge/regexcalculator/controller/RegexCalculatorController.java
@@ -1,6 +1,9 @@
 package com.julian.codechallenge.regexcalculator.controller;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,23 +13,43 @@ import org.springframework.web.bind.annotation.RestController;
 import com.julian.codechallenge.commons.dto.Response;
 import com.julian.codechallenge.regexcalculator.service.RegexCalculatorService;
 
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
+
 @RestController
-@RequestMapping("/calculus")
+@RequestMapping("/regex-calculator")
 public class RegexCalculatorController {
 
     @Autowired
     private RegexCalculatorService regexCalculatorService;
+
+    private final Bucket bucket;
+
+    public RegexCalculatorController() {
+        Bandwidth limit = Bandwidth.classic(20, Refill.greedy(20, Duration.ofMinutes(1)));
+        this.bucket = Bucket4j.builder()
+            .addLimit(limit)
+            .build();
+    }
     
-    @GetMapping
+    @GetMapping("/calculus")
     public ResponseEntity<Response> calculateEncodedQuery(@RequestParam("query") byte[] encodedQuery) {
-        Response response = regexCalculatorService.calculateEncodedQuery(encodedQuery);
-        return ResponseEntity.ok().body(response);
+        if (bucket.tryConsume(1)) {
+            return ResponseEntity.ok(regexCalculatorService.calculateEncodedQuery(encodedQuery));
+        }
+    
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
     }
 
     @GetMapping("/encode")
     public ResponseEntity<byte[]> encodeQuery(@RequestParam("query") String query) {
-        byte[] response = regexCalculatorService.encodeQuery(query);
-        return ResponseEntity.ok().body(response);
+        if (bucket.tryConsume(1)) {
+            return ResponseEntity.ok(regexCalculatorService.encodeQuery(query));
+        }
+    
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
     }
 
 }
